e5e17dd91522532fcc9317700e01e9ad4c44059b
Zuzana Staníčková <staz06@vse.cz>
2026-04-16 11:35:23 +0200

    fix: updated cors url

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 //web protection - authorited domains
 const allowedOrigins = [
     'http://localhost:5173',          
-    'https://bakalarska-prace-delta.vercel.app/' 
+    'https://bakalarska-prace-l067.onrender.com' 
 ];
 
 const corsOptions = {
